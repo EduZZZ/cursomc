41e57a761e4d6a8052297d1db44c6c63324e3ece
eduFeitosa <eduardofeitosa19@icloud.com>
2022-06-08 17:31:48 -0300

    criação banco de dados h2

--- src/main/java/com/eduzzz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/eduzzz/cursomc/resources/CategoriaResource.java
@@ -17,7 +17,7 @@ public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat1 = new Categoria(1, "Informática1");
 		Categoria cat2 = new Categoria(2, "Escritório");
 		
 		List<Categoria> lista = new ArrayList<>();
