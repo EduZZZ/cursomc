a8ad750bdb7233707daac48eecb90b9329ca306f
eduFeitosa <eduardofeitosa19@icloud.com>
2022-06-09 22:51:38 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/eduzzz/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,12 @@
+package com.eduzzz.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduzzz.cursomc.domain.Pagamento;
+
+@Repository
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+		
+}
---
+++ src/main/java/com/eduzzz/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,12 @@
+package com.eduzzz.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduzzz.cursomc.domain.Pedido;
+
+@Repository
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+		
+}
